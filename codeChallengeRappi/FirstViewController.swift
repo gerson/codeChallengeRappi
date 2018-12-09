8d1acb6022b75880ab581a877f6079a9ba6b2816
Gerson Villanueva <faillaceg@gmail.com>
2018-12-09 09:49:31 -0300

    Initial Commit

---
+++ codeChallengeRappi/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  codeChallengeRappi
+//
+//  Created by Gerson Villanueva on 09/12/2018.
+//  Copyright © 2018 rappi. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ codeChallengeRappi/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  codeChallengeRappi
+//
+//  Created by Gerson Villanueva on 09/12/2018.
+//  Copyright © 2018 rappi. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
